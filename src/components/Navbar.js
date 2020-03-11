3d5cbc51ae0fea9d5f7e35509e9f571262137f60
Huzaifah Attah <hhuzaifah050@gmail.com>
2020-03-11 22:11:58 +0000

    Created the Navbar for the pages other than home.

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,30 +1,52 @@
 import React from 'react';
+import logo6 from '../img/logo6.png';
+import { Link, NavLink } from 'react-router-dom';
 
 const Navbar = () => {
   return (
     <div>
-      <nav className="navbar navbar-default login-nav">
-        <div className="container">
-          <div className="navbar-header">
+      <nav class="navbar navbar-default login-nav">
+        <div class="container">
+          <div class="navbar-header">
             <button
               type="button"
-              className="navbar-toggle collapsed"
+              class="navbar-toggle collapsed"
               data-toggle="collapse"
               data-target="#bs-example-navbar-collapse-1"
               aria-expanded="false"
             >
-              <span className="sr-only">Toggle navigation</span>
-              <span className="icon-bar"></span>
-              <span className="icon-bar"></span>
-              <span className="icon-bar"></span>
+              <span class="sr-only">Toggle navigation</span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
             </button>
-            <a className="navbar-brand" href="#">
+            <a class="navbar-brand" href="#">
               <img
-                src="img//logo6.png"
-                className="img-responsive brand-img-login img-circle"
+                src={logo6}
+                class="img-responsive brand-img-login img-circle"
                 alt=""
               />
             </a>
+          </div>
+
+          <div
+            class="collapse navbar-collapse"
+            id="bs-example-navbar-collapse-1"
+          >
+            <ul class="nav navbar-nav navbar-right navbar-right-login">
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/startups">Startups</Link>
+              </li>
+              <li>
+                <Link to="/register">Register</Link>
+              </li>
+              <li>
+                <Link to="/login">Login</Link>
+              </li>
+            </ul>
           </div>
         </div>
       </nav>
