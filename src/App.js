11e52f5dc97330c4928a01f9569c270bceeb9f1c
Huzaifah Attah <hhuzaifah050@gmail.com>
2020-03-12 21:53:16 +0000

    Added startup info route

--- src/App.js
+++ src/App.js
@@ -5,16 +5,16 @@ import { BrowserRouter, Route } from 'react-router-dom';
 import Login from './components/Login';
 import Register from './components/Register';
 import Startups from './components/Startups';
+import Startupinfo from './components/Startupinfo';
 
 function App() {
   return (
     <BrowserRouter>
-      <div>
-        <Route exact={true} path="/" component={Home} />
-        <Route exact={true} path="/login" component={Login} />
-        <Route exact={true} path="/register" component={Register} />
-        <Route exact={true} path="/startups" component={Startups} />
-      </div>
+      <Route exact={true} path="/" component={Home} />
+      <Route exact={true} path="/login" component={Login} />
+      <Route exact={true} path="/register" component={Register} />
+      <Route exact={true} path="/startups" component={Startups} />
+      <Route exact={true} path="/startupinfo" component={Startupinfo} />=
     </BrowserRouter>
   );
 }
