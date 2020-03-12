4523f140cde3d1070a27577751fe0dd047a63bec
Huzaifah Attah <hhuzaifah050@gmail.com>
2020-03-12 21:54:58 +0000

    Added startupinfo rout.

--- src/components/Startups.js
+++ src/components/Startups.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import Footer from './Footer';
 import Navbar from './Navbar';
+import logo1 from '../img/logo1.jpg';
+import logo3 from '../img/logo3.jpg';
+import logo5 from '../img/logo5.jpg';
+import logo2 from '../img/logo2.jpg';
+import { Link } from 'react-router-dom';
 
 export const Startups = () => {
   return (
     <div>
-    <Navbar />
+      <Navbar />
       <div className="container">
         <form>
           <div className="row">
@@ -32,124 +37,89 @@ export const Startups = () => {
           <div className="row startup-search">
             <div className="col-md-2"></div>
             <div className="col-md-2">
-              <img
-                src="img/logo1.jpg"
-                className="img-responsive startup-logo"
-                alt=""
-              />
+              <img src={logo1} className="img-responsive startup-logo" alt="" />
             </div>
             <div className="col-md-6">
               <h3>
-                <a href="startupinfo.html"> tutahub</a>
+                <Link to="/startupinfo"> tutahub</Link>
               </h3>
               <p>a website for tutorials</p>
-              <a
-                href="startupinfo.html"
-                className="btn btn-default startup-btn"
-              >
+              <Link to="/startupinfo" className="btn btn-default startup-btn">
                 See full details
-              </a>
+              </Link>
             </div>
             <div className="col-md-2"></div>
           </div>
           <div className="row startup-search">
             <div className="col-md-2"></div>
             <div className="col-md-2">
-              <img
-                src="img/logo3.jpg"
-                className="img-responsive startup-logo"
-                alt=""
-              />
+              <img src={logo3} className="img-responsive startup-logo" alt="" />
             </div>
             <div className="col-md-6">
               <h3>
-                <a href="startupinfo.html"> Anditer</a>
+                <Link to="/startupinfo"> Anditer</Link>
               </h3>
               <p> Anditer is a tech blog</p>
-              <a
-                href="startupinfo.html"
-                className="btn btn-default startup-btn"
-              >
+              <Link to="/startupinfo" className="btn btn-default startup-btn">
                 See full details
-              </a>
+              </Link>
             </div>
             <div className="col-md-2"></div>
           </div>
           <div className="row startup-search">
             <div className="col-md-2"></div>
             <div className="col-md-2">
-              <img
-                src="img/logo2.jpg"
-                className="img-responsive startup-logo"
-                alt=""
-              />
+              <img src={logo2} className="img-responsive startup-logo" alt="" />
             </div>
             <div className="col-md-8">
               <h3>
-                <a href="startupinfo.html"> Think and Zoom</a>
+                <Link to="/startupinfo"> Think and Zoom</Link>
               </h3>
               <p>
                 {' '}
                 Providing solutions for the visually impaired, such as
                 mind-controlled zooming, and wearable controlled zooming.
               </p>
-              <a
-                href="startupinfo.html"
-                className="btn btn-default startup-btn"
-              >
+              <Link to="/startupinfo" className="btn btn-default startup-btn">
                 See full details
-              </a>
+              </Link>
             </div>
           </div>
           <div className="row startup-search">
             <div className="col-md-2"></div>
             <div className="col-md-2">
-              <img
-                src="img/logo1.jpg"
-                className="img-responsive startup-logo"
-                alt=""
-              />
+              <img src={logo1} className="img-responsive startup-logo" alt="" />
             </div>
             <div className="col-md-8">
               <h3>
-                <a href="startupinfo.html"> Slatecube</a>
+                <Link to="/startupinfo"> Slatecube</Link>
               </h3>
               <p>
                 Slatecube helps job seekers develop job-relevant skills, gain
                 work experience, and land well paying jobs through
                 world-className up-skilling courses and virtual internships.
               </p>
-              <a
-                href="startupinfo.html"
-                className="btn btn-default startup-btn"
-              >
+              <Link to="/startupinfo" className="btn btn-default startup-btn">
                 See full details
-              </a>
+              </Link>
             </div>
           </div>
           <div className="row startup-search">
             <div className="col-md-2"></div>
             <div className="col-md-2">
-              <img
-                src="img/logo5.jpg"
-                className="img-responsive startup-logo"
-                alt=""
-              />
+              <img src={logo5} className="img-responsive startup-logo" alt="" />
             </div>
             <div className="col-md-8">
               <h3>
-                <a href="startupinfo.html"> Sleekjob Academy</a>
+                <Link to="/startupinfo"> Sleekjob Academy</Link>
               </h3>
               <p>
                 Trains world className software developers in Ghana and matches
                 them to employment opportunitie
               </p>
-              <a
-                href="startupinfo.html"
-                className="btn btn-default startup-btn"
-              >
+              <Link to="/startupinfo" className="btn btn-default startup-btn">
                 See full details
-              </a>
+              </Link>
             </div>
           </div>
         </div>
